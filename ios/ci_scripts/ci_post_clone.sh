15b8835c4aff2b397b5916f01b63de11809d552f
SudeepAcharjee <sudeepacharjeegp@gmail.com>
2026-06-10 13:44:33 +0530

    Added

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
+
 set -e
+
+export LANG=en_US.UTF-8
 
 git clone https://github.com/flutter/flutter.git --depth 1 -b stable $HOME/flutter
 
 export PATH="$HOME/flutter/bin:$PATH"
-
-flutter --version
 
 cd "$CI_PRIMARY_REPOSITORY_PATH"
 
