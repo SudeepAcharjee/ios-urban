0db89cd3cfa6dd5d596b29012e6ad29038a49aa4
SudeepAcharjee <sudeepacharjeegp@gmail.com>
2026-06-10 12:06:49 +0530

    Added

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -2,9 +2,22 @@
 
 set -e
 
+echo "Installing Flutter..."
+
+git clone https://github.com/flutter/flutter.git --depth 1 -b stable $HOME/flutter
+
+export PATH="$PATH:$HOME/flutter/bin"
+
+flutter --version
+
 cd "$CI_PRIMARY_REPOSITORY_PATH"
 
 flutter pub get
 
+flutter precache --ios
+
+flutter build ios --debug --no-codesign
+
 cd ios
+
 pod install
